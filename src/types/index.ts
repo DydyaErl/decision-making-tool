7d28a5dc95a196dba876b28f2639bc2f6ef37202
DydyaErl <soyadi6500@gmail.com>
2025-03-14 21:33:21 +0300

    feat: add basic structure and types

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+
+export type Option = {
+    id: string;
+    title: string;
+    weight: string;
+};
+
+
+export type ValidOption = {
+    id: string;
+    title: string;
+    weight: number;
+    color?: string;
+};
+
+export type ElementOptions = {
+    tagName: string;
+    className?: string | string[];
+    textContent?: string;
+    attributes?: Record<string, string>;
+    children?: HTMLElement[];
+    listeners?: Record<string, EventListenerOrEventListenerObject>;
+};
+
+
+export type RouterState = {
+    route: string;
+    params?: Record<string, string>;
+};
+
+
+export type WheelOptions = {
+    options: ValidOption[];
+    duration: number;
+    soundEnabled: boolean;
+};
+
+
+export interface Component {
+    render(): HTMLElement;
+    destroy?(): void;
+}
